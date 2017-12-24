6e1a7930511498d3afa3a58c313fea4922598e63
unknown <degtyarevu@yandex.ru>
2017-12-24 16:16:06 +0300

    10.2 added

---
+++ Homework_10/task10.2/string.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <fstream>
+
+struct String
+{
+    char *data;
+    int size = 0;
+};
+
+bool areEqual(String const &string1, String const &string2);
+
+void input(String &string);
+
+void output(String const &string);
+
+String clone(String const &string);
+
+void cont(String &string, String const &addition);
+
+String getSubstring(String const &string, int first, int last);
+
+void clear(String &string);
+
+char *getCharArray(String const &string);
