4b87dad0d0ceb1aa81c6df76e9c1fb28a1ee5ab6
Degtyarev Ivan <degtyarevu@yandex.ru>
2018-03-09 15:29:59 +0300

    nullpointer checking and codestyle

--- sem2/Hw1/task1.1/Main.java
+++ sem2/Hw1/task1.1/Main.java
@@ -1,16 +1,16 @@
-package com.group144.Degiv;
+package com.group144.degiv;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Stack numbers = new Stack();
 
-        for (int i = 0; i < 9; ++i){
+        for (int i = 0; i < 9; ++i) {
             numbers.add(i);
         }
 
         System.out.println(numbers.GetSize());
 
-        for (int i = 0; i < 9; ++i){
+        for (int i = 0; i < 9; ++i) {
             System.out.println(numbers.pop());
         }
 
--- sem2/Hw1/task1.1/Stack.java
+++ sem2/Hw1/task1.1/Stack.java
@@ -3,20 +3,20 @@ package com.group144.Degiv;
 
 public class Stack {
 
-    private class StackElement{
+    private class StackElement {
         private int value;
         private StackElement next;
 
-        StackElement(int value, StackElement next){
+        StackElement(int value, StackElement next) {
             this.value = value;
             this.next = next;
         }
 
-        public int getValue(){
+        public int getValue() {
             return value;
         }
 
-        public StackElement getNext(){
+        public StackElement getNext() {
             return next;
         }
     }
@@ -24,21 +24,25 @@ public class Stack {
     private StackElement head = null;
     private int size = 0;
 
-    public int GetSize(){
+    public int GetSize() {
         return size;
     }
 
-    public boolean IsEmpty(){
+    public boolean IsEmpty() {
         return size == 0;
     }
 
-    public void add(int value){
+    public void add(int value) {
         head = new StackElement(value, head);
         size += 1;
     }
 
-    public int pop(){
-        int result = head.getValue();
+    public int pop() {
+	int result = -1;
+	if (size == 0)
+		return result;
+
+        result = head.getValue();
         head = head.getNext();
         size -= 1;
         return result;
