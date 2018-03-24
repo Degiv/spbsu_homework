6c8168386f124509e7583c4a14eee4b1889ce58d
Degtyarev Ivan <degtyarevu@yandex.ru>
2018-03-25 02:25:19 +0300

    spaces

--- sem2/Hw2/task2.2/src/main/java/com/group144/degiv/OutputterSpiralConsole.java
+++ sem2/Hw2/task2.2/src/main/java/com/group144/degiv/OutputterSpiralConsole.java
@@ -1,6 +1,6 @@
 package com.group144.degiv;
 
-public class OutputterSpiralConsole extends OutputterSpiral{
+public class OutputterSpiralConsole extends OutputterSpiral {
     @Override
     public void outputSpiral(int[][] numbers) {
         super.outputSpiral(numbers, System.out);
