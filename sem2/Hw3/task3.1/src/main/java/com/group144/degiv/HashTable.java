bbe6b3a157783b8e889f99428aab6835597ccbb1
Degtyarev Ivan <degtyarevu@yandex.ru>
2018-05-13 15:54:13 +0300

    javadoc

--- sem2/Hw3/task3.1/src/main/java/com/group144/degiv/HashTable.java
+++ sem2/Hw3/task3.1/src/main/java/com/group144/degiv/HashTable.java
@@ -3,6 +3,9 @@ package com.group144.degiv;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * class realizing the hash-table
+ */
 public class HashTable {
     private ArrayList<LinkedList<String>> data;
     private HashFunction hashFunction;
@@ -17,6 +20,10 @@ public class HashTable {
         this.hashFunction = hashFunction;
     }
 
+    /**
+     * adds new value in hash-table
+     * @param value new value
+     */
     public void add(String value) {
         int index = hashFunction.hash(value, data.size());
         if (data.get(index).size() == 1) {
@@ -26,6 +33,11 @@ public class HashTable {
         elementCounter++;
     }
 
+    /**
+     * removes value from hash-table
+     * @param value value to remove
+     * @throws ElementNotFoundException
+     */
     public void remove(String value) throws ElementNotFoundException {
         int index = hashFunction.hash(value, data.size());
         if (data.get(index).remove(value)) {
@@ -39,6 +51,11 @@ public class HashTable {
         }
     }
 
+    /**
+     * returns true if table contains the value, returns false otherwise
+     * @param value value to checking
+     * @return true if table contains the value, return false otherwise
+     */
     public boolean contains(String value) {
         return data.get(hashFunction.hash(value, data.size())).contains(value);
     }
@@ -51,6 +68,10 @@ public class HashTable {
         return conflictCounter;
     }
 
+    /**
+     * returns hash-table load factor
+     * @return hash-table load factor
+     */
     public double loadFactor() {
         return (double) getElementCounter() / data.size();
     }
